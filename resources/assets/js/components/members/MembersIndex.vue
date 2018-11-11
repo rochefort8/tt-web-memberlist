8edc4d5842458220f0720871663018b46b7b4298
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2018-11-11 21:42:09 +0900

    Changed search box into "Amazon-style"

--- resources/assets/js/components/members/MembersIndex.vue
+++ resources/assets/js/components/members/MembersIndex.vue
@@ -4,11 +4,28 @@
             <router-link :to="{name: 'createMember'}" class="btn btn-success"><i class ="glyphicon glyphicon-plus"></i>新規登録</router-link>
         </div>
         <form action="" method="POST" role="form">
-	    <div class="form-group">
-                <!-- <label for="">Search</label> -->
-                <input type="text" class="form-control" id="" v-model="search" placeholder="Type content...">
+	          <div class="input-group">
+                <div class="input-group-btn search-panel">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    	    <span id="search_concept">Filter by</span> <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                      <li><a href="#contains">Contains</a></li>
+                      <li><a href="#its_equal">It's equal</a></li>
+                      <li><a href="#greather_than">Greather than ></a></li>
+                      <li><a href="#less_than">Less than < </a></li>
+                      <li class="divider"></li>
+                      <li><a href="#all">Anything</a></li>
+                    </ul>
+                </div>
+                <input type="hidden" name="search_param" value="all" id="search_param">         
+		<input type="text" class="form-control" id="" v-model="search" placeholder="Type content...">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+                </span>
             </div>
         </form>
+
 
 
         <div class="panel panel-default">
